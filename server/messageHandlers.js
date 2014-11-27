ba2151132b13bb2f9408229435e6ad62878818a2
bobbybee <j104@rocketmail.com>
2014-11-27 09:54:14 -0500

    Cast spell server-side infrastructure

--- server/messageHandlers.js
+++ server/messageHandlers.js
@@ -8,7 +8,24 @@ function log(ws, event) {
 	console.log("[ LOG ] ("+ws.logID+"): "+JSON.stringify(event));
 }
 
+function castSpell(ws, event) {
+	if(event.spell == "open sesame") {
+		return {
+			'type': 'spell',
+			'success': false,
+			'info': 'If you are trying to cheat, you really ought to try harder.'
+		};
+	} else {
+		return {
+			'type': 'spell',
+			'success': false,
+			'info': 'A spell must be created by a master venēficī before it is usable by name.'
+		};
+	}
+}
+
 module.exports = {
 	'ping': ping,
 	'log': log,
+	'castSpell': castSpell
 }
