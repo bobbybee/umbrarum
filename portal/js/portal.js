62b0eb61f9e09c05c76ee84166a3dede0b3b513b
bobbybee <j104@rocketmail.com>
2014-11-27 09:03:48 -0500

    ViewManager

---
+++ portal/js/portal.js
@@ -0,0 +1,27 @@
+function ViewManager(defaultView, views) {
+	this.defaultView = defaultView;
+	this.views = views;
+	this.currentView = this.defaultView;
+}
+
+ViewManager.prototype.switchViews = function(newView) {
+	this.hideView(this.currentView);
+	this.currentView = newView;
+	this.showView(this.currentView);
+}
+
+ViewManager.prototype.getViewDiv = function(view) {
+	return document.getElementById('view '+view);
+}
+
+ViewManager.prototype.hideView = function(view) {
+	this.getViewDiv(view).style.display = 'none';
+}
+
+ViewManager.prototype.showView = function(view) {
+	this.getViewDiv(view).style.display = 'block';
+}
+
+window.onload = function() {
+
+}
