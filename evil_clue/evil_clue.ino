4ccee250c1dcab50058843e781aa302a5cc71b50
bobbybee <j104@rocketmail.com>
2014-11-27 13:53:04 -0500

    Ethernet-controlled evil clue

--- evil_clue/evil_clue.ino
+++ evil_clue/evil_clue.ino
@@ -1,4 +1,11 @@
 #include <SoftwareSerial.h>
+#include <SPI.h>
+#include <Ethernet.h>
+
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(192, 168, 1, 177);
+IPAddress server(192, 168, 1, 14);
+EthernetClient client;
 
 SoftwareSerial mySerial(2, 3);
 
@@ -7,14 +14,16 @@ void setup() {
    delay(1000);
    mySerial.write(12);
    mySerial.write(17);
-   
    mySerial.write(128);
-   mySerial.print("Hello, World!");
    
-   mySerial.write(148);
-   mySerial.print("+backlight");
+   Ethernet.begin(mac, ip);
+   delay(1000);
+   client.connect(server, 1234);
 }
 
 void loop() {
-
+  if(client.available()) {
+     char c = client.read();
+     mySerial.write(c);
+  }
 }
