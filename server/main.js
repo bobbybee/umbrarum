8808874d891958344a65e1c9b07ac919124f2c39
bobbybee <j104@rocketmail.com>
2014-11-26 21:39:57 -0500

    Server message handlers

--- server/main.js
+++ server/main.js
@@ -1,3 +1,5 @@
+var messageHandlers = require('./messageHandlers');
+
 var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({
 	port: 8080
@@ -8,6 +10,13 @@ wss.on('connection', function(ws) {
 		try {
 			var event = JSON.parse(message);
 			console.info(event);
+
+			if(messageHandlers[event.type || '']) {
+				ws.send(JSON.stringify(messageHandlers[event.type](ws, event)));
+			} else {
+				console.error("Unknown message type: "+event.type);
+			}
+
 		} catch(e) {
 			console.error(e);
 		}
