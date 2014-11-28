ebeec81dc7db479592f1bdaf2c7437ce7a00cca0
bobbybee <j104@rocketmail.com>
2014-11-28 12:30:40 -0500

    Evil clue as an object

--- server/messageHandlers.js
+++ server/messageHandlers.js
@@ -34,46 +34,46 @@ function log(ws, event) {
 	console.log("[ LOG ] ("+ws.logID+"): "+JSON.stringify(event));
 
 	if(event.event == "switchViews" && gameState.evilClueActive) {
-		evilClueView(event.newView);
+		evilClue.view(event.newView);
 	}
 }
 
-function evilClueView(newView) {
-	if(gameState.evilClueState.currentState == 0) {
-		gameState.evilClueState.currentState++;
-		PeripheralManager.write("evil clue","\x0C\x11\x80");
-		PeripheralManager.write("evil clue","Can't enchant me");
-		PeripheralManager.write("evil clue","\x94Don't waste time");
-	} else if(gameState.evilClueState.currentState == 1 && newView == "wand") {
-		PeripheralManager.write("evil clue","\x0C\x11\x80");
-		PeripheralManager.write("evil clue","Authorization...");
-		PeripheralManager.write("evil clue","\x94DENIED! Go away.");
-		gameState.evilClueState.currentState++;
-	}
-
-}
-
-function revealEvilClue(ws) {
-	PeripheralManager.write("evil clue",'\x0C\x11\x80');
-	PeripheralManager.write("evil clue","OK, master :)");
-	
-	setTimeout(function() {
+var evilClue = {
+	view: function(newView) {
+		if(gameState.evilClueState.currentState == 0) {
+			gameState.evilClueState.currentState++;
+			PeripheralManager.write("evil clue","\x0C\x11\x80");
+			PeripheralManager.write("evil clue","Can't enchant me");
+			PeripheralManager.write("evil clue","\x94Don't waste time");
+		} else if(gameState.evilClueState.currentState == 1 && newView == "wand") {
+			PeripheralManager.write("evil clue","\x0C\x11\x80");
+			PeripheralManager.write("evil clue","Authorization...");
+			PeripheralManager.write("evil clue","\x94DENIED! Go away.");
+			gameState.evilClueState.currentState++;
+		}
+	},
+	reveal: function(ws) {
 		PeripheralManager.write("evil clue",'\x0C\x11\x80');
-		PeripheralManager.write("evil clue","Venefici, summon");
+		PeripheralManager.write("evil clue","OK, master :)");
+		
 		setTimeout(function() {
-			PeripheralManager.write("evil clue","\x94----Clue sent");
-			ws.send(JSON.stringify({
-				'type': 'newClue',
-				'text': 'Lorem ipsum dolor sit amet'
-			}));
-		}, 1000);
-	}, 3000);
+			PeripheralManager.write("evil clue",'\x0C\x11\x80');
+			PeripheralManager.write("evil clue","Venefici, summon");
+			setTimeout(function() {
+				PeripheralManager.write("evil clue","\x94----Clue sent");
+				ws.send(JSON.stringify({
+					'type': 'newClue',
+					'text': 'Lorem ipsum dolor sit amet'
+				}));
+			}, 1000);
+		}, 3000);
+	}
 }
 
 function castSpell(ws, event) {
 	if(event.spell == "pande") {
 		if(gameState.evilClueActive) {
-			revealEvilClue(ws);
+			evilClue.reveal(ws);
 		}
 	} else {
 		return {
