2e5e5717d9517780dfa8c303177acede81170895
bobbybee <j104@rocketmail.com>
2014-11-28 12:33:23 -0500

    evilClue AI is now in its own file

--- server/messageHandlers.js
+++ server/messageHandlers.js
@@ -7,6 +7,8 @@ var gameState = {
 	}
 };
 
+var evilClue = require("./evilClue");
+
 var PeripheralManager = require("./PeripheralManager");
 
 PeripheralManager.start(
@@ -34,42 +36,6 @@ function log(ws, event) {
 
 	if(event.event == "switchViews" && gameState.evilClueActive) {
 		evilClue.view(event.newView);
-	}
-}
-
-var evilClue = {
-	view: function(newView) {
-		if(gameState.evilClueState.currentState == 0) {
-			gameState.evilClueState.currentState++;
-			PeripheralManager.write("evil clue","\x0C\x11\x80");
-			PeripheralManager.write("evil clue","Can't enchant me");
-			PeripheralManager.write("evil clue","\x94Don't waste time");
-		} else if(gameState.evilClueState.currentState == 1 && newView == "wand") {
-			PeripheralManager.write("evil clue","\x0C\x11\x80");
-			PeripheralManager.write("evil clue","Authorization...");
-			PeripheralManager.write("evil clue","\x94DENIED! Go away.");
-			gameState.evilClueState.currentState++;
-		}
-	},
-	reveal: function(ws) {
-		PeripheralManager.write("evil clue",'\x0C\x11\x80');
-		PeripheralManager.write("evil clue","OK, master :)");
-		
-		setTimeout(function() {
-			PeripheralManager.write("evil clue",'\x0C\x11\x80');
-			PeripheralManager.write("evil clue","Venefici, summon");
-			setTimeout(function() {
-				PeripheralManager.write("evil clue","\x94----Clue sent");
-				ws.send(JSON.stringify({
-					'type': 'newClue',
-					'text': 'Lorem ipsum dolor sit amet'
-				}));
-			}, 1000);
-		}, 3000);
-	},
-	connect: function() {
-		PeripheralManager.write("evil clue", "I have the clue.");
-		PeripheralManager.write("evil clue", "\x94You don't. Haha.");
 	}
 }
 
