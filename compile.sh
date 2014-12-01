7ee7b53ef0a02e0f8877d91768c9c3f971f65ffd
bobbybee <j104@rocketmail.com>
2014-11-30 20:20:39 -0500

    Fix a bug from the minifer causing it to bork the portal size

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@
 # https://github.com/jakubpawlowicz/clean-css
 
 export COMMIT=`git log --pretty=format:'%h' -n 1`
-html-minifier portal/index.html --remove-comments --remove-comments-from-cdata --collapse-whitespace --remove-attribute-quotes --remove-redundant-attributes --use-short-doctype --remove-empty-attributes  --remove-optional-tags --remove-empty-elements --minify-js --minify-css > ../umbrarum.github.io/index.html
+html-minifier portal/index.html --remove-comments --collapse-whitespace --minify-js --minify-css > ../umbrarum.github.io/index.html
 uglifyjs portal/js/client.js --screw-ie8 --mangle --compress --lint -o ../umbrarum.github.io/js/client.js
 uglifyjs portal/js/portal.js --screw-ie8 --mangle --compress --lint -o ../umbrarum.github.io/js/portal.js
 uglifyjs portal/js/viewmanager.js --screw-ie8 --mangle --compress --lint -o ../umbrarum.github.io/js/viewmanager.js
