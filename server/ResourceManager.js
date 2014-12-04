77f3475f9631ea6462021e29a999d31e0369e9c7
jczimm <jczimm.alias@gmail.com>
2014-12-03 22:02:39 -0500

    fixed dir name

--- server/ResourceManager.js
+++ server/ResourceManager.js
@@ -4,8 +4,8 @@ console.log("ResourceManager loading resources. This may take a moment..")
 
 var resources = (function(read, list) {
     var obj = {};
-    list("rsrc/").forEach(function(uuid) {
-        obj[uuid.replace('.rsrc','')] = read("rsrc/cr0yGHWDGA.rsrc").toString();
+    list("server/rsrc/").forEach(function(uuid) {
+        obj[uuid.replace('.rsrc','')] = read("server/rsrc/cr0yGHWDGA.rsrc").toString();
     });
     return obj;
 })(fs.readFileSync, fs.readdirSync);
