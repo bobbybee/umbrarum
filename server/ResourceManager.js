006aa6f01e1548e6829e810b56e94bd083921c35
mytasks <platypusnetwork@gmail.com>
2014-12-03 21:58:30 -0500

    made resource manager "algorithmic", and made logs more useful

--- server/ResourceManager.js
+++ server/ResourceManager.js
@@ -2,12 +2,16 @@ var fs = require('fs');
 
 console.log("ResourceManager loading resources. This may take a moment..")
 
-var resources = {
-	'cr0yGHWDGA': fs.readFileSync("rsrc/cr0yGHWDGA.rsrc").toString()
-};
+var resources = (function(read, list) {
+    var obj = {};
+    list("rsrc/").forEach(function(uuid) {
+        obj[uuid.replace('.rsrc','')] = read("rsrc/cr0yGHWDGA.rsrc").toString();
+    });
+    return obj;
+})(fs.readFileSync, fs.readdirSync);
 
 console.log("Done!");
 
 module.exports.get = function(uuid) {
-	return resources[uuid];
+    return resources[uuid];
 }
--- server/register.js
+++ server/register.js
@@ -1,7 +1,11 @@
 var fs = require('fs');
 
 module.exports = function(ws, event) {
-	fs.appendFile('enrollment.txt', event.name+"\n"+event.message+"\n"+"------"+"\n", function(){});
+	fs.appendFile('enrollment.txt',
+		event.name + "\n" +
+		event.message + "\n" +
+		"------" + "\n"
+	, function(){});
 
 	return {
 		type: "registered"
