432df2b9114eaf384af1aaa560d8fdc5fee6062b
jczimm <jczimm.alias@gmail.com>
2014-12-17 15:02:03 -0500

    added comments about hardware in arduino-based clues (TODO: phone.ino)

---
+++ arduino/game_clue/game_clue.ino
@@ -0,0 +1,16 @@
+/*
+
+HARDWARE:
+ - Arduino UNO R3
+ - Arduino Ethernet Shield for UNO
+ - Parallax Serial LCD
+   
+Connections:
+ [ Arduino ] [ Serial LCD ]
+ -   GND   to   GND
+ -   5V    to   5V
+ -   Pin 3 to   RX
+ 
+*/
+
+// Parallax Serial LCD documentation at http://elmicro.com/files/parallax/seriallcd-v20.pdf
