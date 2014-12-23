d1a9272cf6a3343ea6b27dbce80abb46c2ee9236
bobbybee <j104@rocketmail.com>
2014-12-22 19:22:27 -0500

    little bug. other meaning tho haha

--- server/ResourceManager.js
+++ server/ResourceManager.js
@@ -5,7 +5,7 @@ console.log("ResourceManager loading resources. This may take a moment..")
 var resources = (function(read, list) {
     var obj = {};
     list("server/rsrc/").forEach(function(uuid) {
-        obj[uuid.replace('.rsrc','')] = read("server/rsrc/cr0yGHWDGA.rsrc").toString();
+        obj[uuid.replace('.rsrc','')] = read("server/rsrc/"+uuid).toString();
     });
     return obj;
 })(fs.readFileSync, fs.readdirSync);
